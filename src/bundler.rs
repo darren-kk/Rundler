e7f25308b414afb651aa3bfbc96da8b1ea87fcf1
darren-kk <rlawoghks10@gmail.com>
2023-12-25 19:37:49 +0900

    Feat. add parsing imported  module function

--- src/bundler.rs
+++ src/bundler.rs
@@ -18,3 +18,38 @@ fn parse_iterate_module<F: FnMut(&SyntaxNode) -> bool>(content: &String, cb: &mu
       }
   }
 }
+
+
+fn parse_module_imports(content: &String) -> Vec<String> {
+  let mut sources = Vec::new();
+  let mut _iter = |_node: &SyntaxNode| -> bool {
+      if _node.kind() == SyntaxKind::IMPORT_DECL {
+          let mut _import_node = _node.first_child();
+
+          'import: loop {
+              while let Some(_in) = _import_node {
+                  if _in.kind() == SyntaxKind::LITERAL {
+                      let src = _in
+                          .text()
+                          .to_string()
+                          .replace(&['\'', '\"', ' ', '\t'][..], "")
+                          .to_owned();
+
+                      println!("src:{:?}", &src);
+                      sources.push(src);
+
+                      break 'import;
+                  }
+
+                  _import_node = _in.next_sibling();
+              }
+          }
+      }
+
+      return true;
+  };
+
+  parse_iterate_module(content, &mut _iter);
+
+  return sources;
+}
