0712797f8860a0abb642cd6f5cf839cd0c6e354d
darren-kk <rlawoghks10@gmail.com>
2023-12-26 12:29:15 +0900

    Feat. add making combined module struct with its dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use rslint_parser::{parse_module, SyntaxKind, SyntaxNode};
 use path_absolutize::*;
+use rslint_parser::{parse_module, SyntaxKind, SyntaxNode};
 
 use std::path::Path;
 use std::fs;
@@ -17,71 +17,9 @@ fn main() {
     let entry_point = args.get(1).expect("No entry point specified");
     let abs_path = change_to_abs_path(entry_point);
 
-    parse_to_ast(entry_point)
-}
+    let result = new_module(&abs_path);
 
-fn parse_iterate_module<F: FnMut(&SyntaxNode) -> bool>(content: &String, cb: &mut F) -> () {
-    let parse = parse_module(content, 0);
-    let mut syntax_node = parse.syntax().first_child();
-  
-    // println!("parsed AST: {:?}", parse);
-  
-    loop {
-        let mut _node = syntax_node.unwrap();
-        let cont = cb(&_node);
-  
-        if !cont {
-            break;
-        }
-  
-        syntax_node = match _node.next_sibling() {
-            Some(next) => Some(next),
-            _ => break,
-        }
-    }
-  }
-  
-  
-  fn parse_module_imports(content: &String) -> Vec<String> {
-    let mut sources = Vec::new();
-    let mut _iter = |_node: &SyntaxNode| -> bool {
-        if _node.kind() == SyntaxKind::IMPORT_DECL {
-            let mut _import_node = _node.first_child();
-  
-            'import: loop {
-                while let Some(_in) = _import_node {
-                    if _in.kind() == SyntaxKind::LITERAL {
-                        let src = _in
-                            .text()
-                            .to_string()
-                            .replace(&['\'', '\"', ' ', '\t'][..], "")
-                            .to_owned();
-  
-                        println!("src:{:?}", &src);
-                        sources.push(src);
-  
-                        break 'import;
-                    }
-  
-                    _import_node = _in.next_sibling();
-                }
-            }
-        }
-  
-        return true;
-    };
-  
-    parse_iterate_module(content, &mut _iter);
-  
-    return sources;
-  }
-  
-
-fn parse_to_ast(file_path: &String) {
-    let contents = fs::read_to_string(file_path).expect("error reading");
-    let a = parse_module_imports(&contents);
-
-    println!("this is imported {:?}", a);
+    println!("result {:?}", result);
 }
 
 fn change_to_abs_path(file_path: &String) -> String {
@@ -91,4 +29,83 @@ fn change_to_abs_path(file_path: &String) -> String {
     println!("Absolute path: {}", absolute_path);
 
     absolute_path
+}
+
+fn new_module(file_path: &String) -> Module {
+    let abs_path = change_to_abs_path(file_path);
+    let contents = fs::read_to_string(file_path).expect("error reading");
+    let imports = parse_module_imports(&contents);
+    let mut mods = Vec::new();
+
+    for import in imports {
+        // get dirname + imported file path
+        let path = Path::new(file_path).parent().unwrap().join(import);
+        match path.to_str() {
+            Some(s) => mods.push(new_module(&String::from(s))),
+            None => panic!("cannot convert path to string"),
+        }
+    }
+
+    println!("module complete: {abs_path}");
+
+    return Module {
+        file_path: abs_path,
+        module_content: contents,
+        dependencies: mods,
+    };
+}
+
+fn parse_iterate_module<F: FnMut(&SyntaxNode) -> bool>(content: &String, cb: &mut F) -> () {
+    let parse = parse_module(content, 0);
+    let mut syntax_node = parse.syntax().first_child();
+
+    // println!("parsed AST: {:?}", parse);
+
+    loop {
+        let mut _node = syntax_node.unwrap();
+        let cont = cb(&_node);
+
+        if !cont {
+            break;
+        }
+
+        syntax_node = match _node.next_sibling() {
+            Some(next) => Some(next),
+            _ => break,
+        }
+    }
+}
+
+fn parse_module_imports(content: &String) -> Vec<String> {
+    let mut sources = Vec::new();
+    let mut _iter = |_node: &SyntaxNode| -> bool {
+        if _node.kind() == SyntaxKind::IMPORT_DECL {
+            let mut _import_node = _node.first_child();
+
+            'import: loop {
+                while let Some(_in) = _import_node {
+                    if _in.kind() == SyntaxKind::LITERAL {
+                        let src = _in
+                            .text()
+                            .to_string()
+                            .replace(&['\'', '\"', ' ', '\t'][..], "")
+                            .to_owned();
+
+                        println!("src:{:?}", &src);
+                        sources.push(src);
+
+                        break 'import;
+                    }
+
+                    _import_node = _in.next_sibling();
+                }
+            }
+        }
+
+        return true;
+    };
+
+    parse_iterate_module(content, &mut _iter);
+
+    return sources;
 }
